098dfa20bf28b756122ac39f6257370cdcf57448
Dahrunning <105024702+Dahrunning@users.noreply.github.com>
2023-02-02 02:31:23 +0000

    i added task 9 and 10

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ let displayLink = document.querySelector(".display-link");
 
 
 // task 7: create and populate an array to store your image urls. Create three more arrays. One to store your song names, one for the artists, and a last one for the song links.
-let images = [ "https://upload.wikimedia.org/wikipedia/en/9/97/DittoNewJeans.jpg","https://i.scdn.co/image/ab67616d00001e0287f53da5fb4ab1171766b2d5", "https://i.scdn.co/image/ab67616d0000b273b88a1e33abc02c7c919c909b", "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Everything_Goes_On_single_cover.jpg/220px-Everything_Goes_On_single_cover.jpg"];
-let songNames = [ "Ditto", "After Like", "Cinderella", "Everything Goes On", ];
-let artists = [ "NewJeans", "IVE", "DECO*27", "Porter Robinson", ];
-let songLinks = [ "https://www.youtube.com/watch?v=-g9I2neQR7w", "https://www.youtube.com/watch?v=CVxTT38_J4c","https://www.youtube.com/watch?v=adGhT_-JbZI", "https://www.youtube.com/watch?v=eEFVxI9lqjU", ];
+let images = ["https://upload.wikimedia.org/wikipedia/en/9/97/DittoNewJeans.jpg", "https://i.scdn.co/image/ab67616d00001e0287f53da5fb4ab1171766b2d5", "https://i.scdn.co/image/ab67616d0000b273b88a1e33abc02c7c919c909b", "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Everything_Goes_On_single_cover.jpg/220px-Everything_Goes_On_single_cover.jpg"];
+let songNames = ["Ditto", "After Like", "Cinderella", "Everything Goes On",];
+let artists = ["NewJeans", "IVE", "DECO*27", "Porter Robinson",];
+let songLinks = ["https://www.youtube.com/watch?v=-g9I2neQR7w", "https://www.youtube.com/watch?v=CVxTT38_J4c", "https://www.youtube.com/watch?v=adGhT_-JbZI", "https://www.youtube.com/watch?v=eEFVxI9lqjU",];
 
 
 
@@ -49,11 +49,20 @@ let songLinks = [ "https://www.youtube.com/watch?v=-g9I2neQR7w", "https://www.yo
 
 function addSongInfo() {
 
-// task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
+  // task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
+
+  let userImg = image.value;
+  let userSong = songName.value;
+  let userArtist = artist.value;
+  let userLink = songLink.value;
 
 
-// task 10: use `.push()` to add each input value to the correct array.
+  // task 10: use `.push()` to add each input value to the correct array.
 
+  images.push(userImg);
+  songNames.push(userSong);
+  artists.push(userArtist);
+  songLinks.push(userLink);
 }
 
 
@@ -65,39 +74,39 @@ function emptyDisplay() {
   displaySong.innerHTML = "";
   displayArtist.innerHTML = "";
   displayLink.innerHTML = "";
+
 }
 
 
 
 
 function displaySongInfo() {
+  // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
 
-// task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
+  for (let i = 0; i < images.length; i++) {
+    displayImage.insertAdjacentHTML('beforeend', `<p><img src=${images[i]}></p>`)
+  }
 
-for(let i = 0; i < images.length; i++){
-  displayImage.insertAdjacentHTML('beforeend',`<p><img src=${images[i]}></p>`)
-} 
+  for (let j = 0; j < songNames.length; j++) {
+    displaySong.insertAdjacentHTML('beforeend', `<p>${songNames[j]}</p>`)
+  }
+
+  for (let i = 0; i < artists.length; i++) {
+    displayArtist.insertAdjacentHTML('beforeend', `<p>${artists[i]}</p>`)
+  }
+
+  for (let i = 0; i < songLinks.length; i++) {
+    displayLink.insertAdjacentHTML('beforeend', `<a href="${songLinks[i]}"> Link ! Click Me ! </a>`)
+  }
+
 };
-
-for(let i = 0; i < songNames.length; i++){
-  displaySong.insertAdjacentHTML('beforeend',`<p>${songNames[i]}</p>`)
-}
-
-for(let i = 0; i < artists.length; i++){
-  displayArtist.insertAdjacentHTML('beforeend',`<p>${artists[i]}</p>`)
-}
-
-for(let i = 0; i < songLinks.length; i++){
-  displayLink.insertAdjacentHTML('beforeend',`<a href = "${songLinks[i]}"> Link ! Click Me ! </a>`)
-}
-
-
 
 
 
 // click event to add and display songs
 add.onclick = function() {
   addSongInfo();
+  emptyDisplay();
   displaySongInfo();
 };
 
