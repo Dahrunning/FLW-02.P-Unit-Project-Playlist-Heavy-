f2026d0c7a5856231b26ce0194e6b6f65e8e60ab
Dahrunning <105024702+Dahrunning@users.noreply.github.com>
2023-02-09 02:23:13 +0000

    arrays have been changed!

--- script.js
+++ script.js
@@ -33,8 +33,33 @@ let songLinks = ["https://www.youtube.com/watch?v=-g9I2neQR7w", "https://www.you
 //REFACTOR ARRAYS DAY 
 // task 11: comment out the arrays data.
 // task 12: create an object for each of your songs.
+let song1 = {
+  image: "https://upload.wikimedia.org/wikipedia/en/9/97/DittoNewJeans.jpg",
+  songName: "Ditto",
+  artist: "NewJeans",
+  songLink: "https://www.youtube.com/watch?v=-g9I2neQR7w",
+};
+let song2 = {
+  image: "https://i.scdn.co/image/ab67616d00001e0287f53da5fb4ab1171766b2d5",
+  songName: "After Like",
+  artist: "IVE",
+  songLink: "https://www.youtube.com/watch?v=CVxTT38_J4c",
+};
+let song3 = {
+  image: "https://i.scdn.co/image/ab67616d0000b273b88a1e33abc02c7c919c909b",
+  songName: "Cinderella",
+  artist: "DECO*27",
+  songLink: "https://www.youtube.com/watch?v=adGhT_-JbZI",
+};
+let song4 = {
+  image: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a7/Everything_Goes_On_single_cover.jpg/220px-Everything_Goes_On_single_cover.jpg",
+  songName: "Everything Goes On",
+  artist: "Porter Robinson",
+  songLink: "https://www.youtube.com/watch?v=eEFVxI9lqjU",
+};
 // task 13: inside each object, add key/value pairs to store the image url, song name, artist, and song link.
 // task 14: create an array that stores all of the objects.
+let objects = [song1, song2, song3, song4]
 
 
 
@@ -51,18 +76,24 @@ function addSongInfo() {
 
   // task 9: declare a variable to save the user input of the image url. Declare three more variables that save user input: One for the song names, one for the artists, and a last one for the song links.
 
-  let userImg = image.value;
-  let userSong = songName.value;
-  let userArtist = artist.value;
-  let userLink = songLink.value;
+  let newSong = {
+    image: image.value,
+    songName: songName.value,
+    artist: artist.value,
+    songLink: songLink.value,
+  }
+
+
 
 
   // task 10: use `.push()` to add each input value to the correct array.
 
-  images.push(userImg);
-  songNames.push(userSong);
-  artists.push(userArtist);
-  songLinks.push(userLink);
+  objects.push(newSong);
+
+  // images.push(userImg);
+  // songNames.push(userSong);
+  // artists.push(userArtist);
+  // songLinks.push(userLink);
 }
 
 
@@ -83,21 +114,21 @@ function emptyDisplay() {
 function displaySongInfo() {
   // task 8: loop through your images array and display the images to your songs in the correct div. Create three more loops. One for the song names, one for the artists, and a last one for the song links.
 
-  for (let i = 0; i < images.length; i++) {
-    displayImage.insertAdjacentHTML('beforeend', `<p><img src=${images[i]}></p>`)
-  }
+  //   for (let i = 0; i < images.length; i++) {
+  //     displayImage.insertAdjacentHTML('beforeend', `<p><img src=${images[i]}></p>`)
+  //   }
 
-  for (let j = 0; j < songNames.length; j++) {
-    displaySong.insertAdjacentHTML('beforeend', `<p>${songNames[j]}</p>`)
-  }
+  //   for (let j = 0; j < songNames.length; j++) {
+  //     displaySong.insertAdjacentHTML('beforeend', `<p>${songNames[j]}</p>`)
+  //   }
 
-  for (let i = 0; i < artists.length; i++) {
-    displayArtist.insertAdjacentHTML('beforeend', `<p>${artists[i]}</p>`)
-  }
+  //   for (let i = 0; i < artists.length; i++) {
+  //     displayArtist.insertAdjacentHTML('beforeend', `<p>${artists[i]}</p>`)
+  //   }
 
-  for (let i = 0; i < songLinks.length; i++) {
-    displayLink.insertAdjacentHTML('beforeend', `<a href="${songLinks[i]}"> Link ! Click Me ! </a>`)
-  }
+  //   for (let i = 0; i < songLinks.length; i++) {
+  //     displayLink.insertAdjacentHTML('beforeend', `<a href="${songLinks[i]}"> Link ! Click Me ! </a>`)
+  //   }
 
 };
 
